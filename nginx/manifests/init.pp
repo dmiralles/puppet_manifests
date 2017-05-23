89063a472ec4d8c2e31ac7672a1c261e1106ce9b
David <davidmifo@gmail.com>
2017-05-23 11:02:39 +0200

    Second commit
    Changes on nginx configuration files

--- nginx/manifests/init.pp
+++ nginx/manifests/init.pp
@@ -1,5 +1,12 @@
-class nginx {
-        package {'nginx':
-                ensure => present,
-        }
+class nginx (
+    $nginx_config_dir  ='/etc/nginx/sites-available',
+    $wp_directory      ='/var/www/html') {
+    package {'nginx':
+        ensure => present,
+    }
+    file { "nginx_config_dir/default":
+            ensure  => present,
+            mode    => '0644',
+            content => template("nginx/default.erb"),
+    }
 }
